d6210941495b05f6a4d7f3d91f0e4165b605b376
Sanchous <axanutin@mail.ru>
2023-05-23 14:47:22 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,28 @@
+package ru.netology
+
+import kotlin.math.min
+
+const val ERROR_CARD = -1
+const val ERROR_LIMIT = -2
+
+fun main() {
+    println(commission("VK Pay", 0, 500))
+}
+
+fun commission(typeCard: String, previous: Int, transfer: Int): Int = when(typeCard) {
+    "MasterCard", "Maestro" -> when {
+        transfer > 150_000 || transfer + previous > 600_000 -> ERROR_LIMIT
+        transfer < 75000 -> 0
+        else -> 20 + (transfer * 0.006).toInt()
+    }
+    "VK Pay" -> when {
+        transfer > 15_000 || transfer + previous > 40_000 -> ERROR_LIMIT
+        else -> 0
+    }
+    "Visa", "Мир" -> when {
+        transfer > 150_000 || transfer + previous > 600_000 -> ERROR_LIMIT
+        transfer * 0.0075 < 35 -> 35
+        else -> (transfer * 0.0075).toInt()
+    }
+    else -> ERROR_CARD
+}
